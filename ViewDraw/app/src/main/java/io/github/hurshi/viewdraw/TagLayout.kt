4be9bd90d2ff406c679d022982ba08656071c3b1
Hurshi <ihsruh@gmail.com>
2019-04-23 12:00:03 +0800

    fix TagLayout

--- ViewDraw/app/src/main/java/io/github/hurshi/viewdraw/TagLayout.kt
+++ ViewDraw/app/src/main/java/io/github/hurshi/viewdraw/TagLayout.kt
@@ -2,7 +2,6 @@ package io.github.hurshi.viewdraw
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
 import android.view.ViewGroup
 
 class TagLayout : ViewGroup {
@@ -12,87 +11,46 @@ class TagLayout : ViewGroup {
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
         val widthSize = MeasureSpec.getSize(widthMeasureSpec)
         val widthMode = MeasureSpec.getMode(widthMeasureSpec)
 
-        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
-        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        var widthUsed = 0
+        var heightUsed = 0
+
+        var maxHeightInSingleLine = 0
 
         (0 until childCount).forEach { childViewIndex ->
             val v = getChildAt(childViewIndex)
-            val lp = v.layoutParams
-            var cWidthSize: Int
-            var cWidthMode: Int
-            var cHeightSize: Int
-            var cHeightMode: Int
+            measureChildWithMargins(v, widthMeasureSpec, widthUsed, heightMeasureSpec, heightUsed)
 
-            when (lp.width) {
-                LayoutParams.WRAP_CONTENT -> {
-                    cWidthSize = widthSize
-                    cWidthMode = MeasureSpec.AT_MOST
-                }
-                LayoutParams.MATCH_PARENT -> {
-                    cWidthSize = widthSize
-                    cWidthMode = MeasureSpec.EXACTLY
-                }
-                else -> {
-                    cWidthSize = lp.width
-                    cWidthMode = MeasureSpec.EXACTLY
-                }
-            }
+            val lp = v.layoutParams as MarginLayoutParams
+            val measuredChildWidthWithMargin = v.measuredWidth + lp.leftMargin + lp.rightMargin
 
-            when (lp.height) {
-                LayoutParams.WRAP_CONTENT -> {
-                    cHeightSize = heightSize
-                    cHeightMode = MeasureSpec.AT_MOST
-                }
-                LayoutParams.MATCH_PARENT -> {
-                    cHeightSize = heightSize
-                    cHeightMode = MeasureSpec.EXACTLY
-                }
-                else -> {
-                    cHeightSize = lp.height
-                    cHeightMode = MeasureSpec.EXACTLY
-                }
+            //如果子 View 测量后总宽度超过了本 View 的宽度，则进行换行，然后再次测量
+            if ((widthMode == MeasureSpec.EXACTLY || widthMode == MeasureSpec.AT_MOST)
+                && (paddingLeft + paddingRight + widthUsed + measuredChildWidthWithMargin) > widthSize
+            ) {
+                heightUsed += maxHeightInSingleLine
+                maxHeightInSingleLine = 0
+                widthUsed = 0
+                measureChildWithMargins(v, widthMeasureSpec, widthUsed, heightMeasureSpec, heightUsed)
             }
-            measureChild(v, cWidthSize, cWidthMode, cHeightSize, cHeightMode)
+            maxHeightInSingleLine =
+                Math.max(maxHeightInSingleLine, v.measuredHeight + lp.topMargin + lp.bottomMargin)
+            widthUsed += measuredChildWidthWithMargin
+
+            v.left = widthUsed - measuredChildWidthWithMargin + paddingLeft
+            v.right = widthUsed - lp.rightMargin + paddingLeft
+            v.top = heightUsed + lp.topMargin + paddingTop
+            v.bottom = heightUsed + lp.topMargin + v.measuredHeight + paddingTop
         }
     }
 
-    private fun measureChild(v: View, widthSize: Int, widthMode: Int, heightSize: Int, heightMode: Int) {
-        v.measure(
-            MeasureSpec.makeMeasureSpec(widthSize, widthMode),
-            MeasureSpec.makeMeasureSpec(heightSize, heightMode)
-        )
-    }
-
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        var widthUsed = 0
-        var heightUsed = 0
-        val allWidth = measuredWidth - paddingLeft - paddingStart - paddingRight - paddingEnd
-        var maxHeightInSingleLine = 0
         (0 until childCount).forEach { childViewIndex ->
             val v = getChildAt(childViewIndex)
-            val lp = v.layoutParams as MarginLayoutParams
-            val layoutChild = {
-                v.layout(
-                    paddingLeft + widthUsed + lp.leftMargin,
-                    paddingTop + heightUsed + lp.topMargin,
-                    paddingLeft + widthUsed + lp.leftMargin + v.measuredWidth,
-                    paddingTop + heightUsed + lp.topMargin + v.measuredHeight
-                )
-                widthUsed += (v.measuredWidth + lp.leftMargin + lp.rightMargin)
-                maxHeightInSingleLine =
-                    Math.max(maxHeightInSingleLine, v.measuredHeight + lp.topMargin + lp.bottomMargin)
-            }
-            if (v.measuredWidth + lp.leftMargin + lp.rightMargin <= allWidth - widthUsed) {
-                layoutChild()
-            } else {
-                widthUsed = 0
-                heightUsed += maxHeightInSingleLine
-                maxHeightInSingleLine = 0
-                layoutChild()
-            }
+            v.layout(v.left, v.top, v.right, v.bottom)
         }
     }
 
